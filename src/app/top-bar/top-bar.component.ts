b6e76649044fab575bae6da687fca8f7547d58e0
Wataru.Kasahara <kasahara1019@gmail.com>
2022-06-05 09:28:00 +0900

    yarn ng g c top-bar --standalone

---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-top-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.scss']
+})
+export class TopBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
