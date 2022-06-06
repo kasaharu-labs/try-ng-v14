0ce6ffa14fb230fbe0f59a88135f44f8e92f4de9
Wataru.Kasahara <kasahara1019@gmail.com>
2022-06-06 22:59:05 +0900

    feat: root component を standalone component に変更

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterModule, TopBarComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'try-ng-v14';
