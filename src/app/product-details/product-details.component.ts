945d95b5d168c2a432357b8029bcc79cf89f523d
Wataru.Kasahara <kasahara1019@gmail.com>
2022-06-09 21:40:28 +0900

    feat: product 詳細ページを作成

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Product, products } from '../product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
 
-  constructor() { }
+  product: Product | undefined;
 
   ngOnInit(): void {
-  }
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
 }
