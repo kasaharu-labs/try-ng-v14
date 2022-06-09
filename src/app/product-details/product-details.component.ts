58606d5de2f3eb9a9f82478a9eef5b33eb110449
Wataru.Kasahara <kasahara1019@gmail.com>
2022-06-09 20:57:29 +0900

    yarn ng generate component product-details

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
